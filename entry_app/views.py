0bc5f9ea872cadc9f6b48a605f275bef37cb9e7b
Satbir Singh <satbirform@gmail.com>
2019-11-28 18:00:30 +0530

    Added sms feature using Twilio API

--- entry_app/views.py
+++ entry_app/views.py
@@ -1,11 +1,19 @@
 
-
+from twilio.rest import Client
 from django.conf import settings
 from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.template import loader
 from entry_app.forms import HostForm, VisitorForm, Checkout
 from entry_app.models import Visitor,Host
+
+
+account_sid = 'AC4fae24bda331fbeae76ceefc021cbb06' # Found on Twilio Console Dashboard
+auth_token = '8d95bfe44d93f2557783f691f2c9b367' # Found on Twilio Console Dashboard
+
+myPhone = '+918299698057' # Phone number you used to verify your Twilio account
+TwilioNumber = '+19513632557' # Phone number given to you by Twilio
+
 
 
 def index(request):
@@ -32,6 +40,8 @@ def hosts_form(request):
     return HttpResponse(template.render(context, request))
 
 def visitor_form(request):
+    client = Client(account_sid, auth_token)
+
     template = loader.get_template('vform.html')
     form_object = VisitorForm()
     if request.method == 'POST':
@@ -45,6 +55,7 @@ def visitor_form(request):
             last_val=Visitor.objects.last()
             message='\nName : '+str(entered_name)+'\n Email : '+str(entered_email)+'\n Phone : '+str(entered_phone)+'\n Checkin : '+str(last_val.check_in)
             send_mail(subject="Visitor Entry Details",from_email=settings.EMAIL_HOST_USER,message=message,recipient_list=[current_host.email,'entry.manage.satbir@gmail.com'],auth_password=settings.EMAIL_HOST_PASSWORD)
+            client.messages.create(to=current_host.phone,from_=TwilioNumber,body=message + u'\U0001f680')
             return index(request)
     context = {
         'form_full': form_object
